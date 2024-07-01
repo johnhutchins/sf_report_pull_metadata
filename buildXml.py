d329dc7b5b9f9aff3794fd6668d40219dea8f31c
john <>
2024-07-01 10:21:49 -0400

    - each member to new line. - update read me

--- buildXml.py
+++ buildXml.py
@@ -1,20 +1,28 @@
 import pandas as pd
 import xml.etree.ElementTree as ET
 
+# Load the JSON file into a DataFrame
 json_file_path = 'ReportFolderData.json'
-
 df = pd.read_json(json_file_path)
 
+# Create the root element for the XML
 root = ET.Element('root')
 
+# Iterate over the rows in the DataFrame and create the XML structure
 for index, row in df.iterrows():
     member_element = ET.SubElement(root, 'members')
     member_element.text = f"{row['folderName']}/{row['reportDeveloperName']}"
 
+# Convert the ElementTree to a string and add line breaks
 tree = ET.ElementTree(root)
+tree_string = ET.tostring(root, encoding='unicode')
 
+# Adding line breaks between members elements
+tree_string_with_breaks = tree_string.replace('</members>', '</members>\n')
+
+# Write the formatted XML string to a file
 output_xml_path = 'FormattedStringsOutput.xml'
+with open(output_xml_path, 'w') as xml_file:
+    xml_file.write(tree_string_with_breaks)
 
-tree.write(output_xml_path)
-
-print(f"Formatted XML strings written to {output_xml_path}")
+print(f"Formatted XML strings with line breaks written to {output_xml_path}")
--- readme.md
+++ readme.md
@@ -1,9 +1,14 @@
+# Important: make sure that inside vscode we search and filter out anything other than reports
+
 ## Steps to get all report meta data:
 
 - Run getFolders.apex anonymously to generate the file
 - Search for the file in the global search to find it and download it
 - Download the file and save to a location you can reference in your Python script
 - Run the python script to generate the xml
+- Remove the <root> element from the generated XML (issue should be logged and fixed eventually)
+- Add a <name>Report</name> element at the bottom of the generated XML (issue should be logged and fixed eventually)
 - Use the XML to update package.xml in the org directory
+- Right click on package.xml and "Retrieve source from org"
 
 ## TODO update python script to remove the "root" element
